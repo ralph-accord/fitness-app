8f27451b701119184e8d57161d129f04e9fc3fa7
ralph-accord <ralph@inaccord.com>
2024-01-05 10:30:18 -0500

    feat(webapp): starter code

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome to Fitness App!</h1>
+      <p>Get fit and stay healthy with our amazing fitness app.</p>
+    </div>
+  );
+};
+
+export default Welcome;
